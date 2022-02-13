f7ee28e6c042642e7026aafa2bb2d798e1f4c012
yj95228 <kei02119@naver.com>
2022-02-13 10:54:03 +0900

    Add home and movie-detail Pages

---
+++ src/Button.js
@@ -0,0 +1,11 @@
+import PropTypes from "prop-types";
+import styles from "./Button.module.css";
+
+function Button({text, onClick}){
+    return <button onClick={onClick} className={styles.btn}>{text}</button>;
+}
+
+Button.propTypes = {
+    text: PropTypes.string.isRequired,
+};
+export default Button;
